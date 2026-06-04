854aa24d63aa5fe51d868dcc6f588d36ee252d6f
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2026-06-04 08:20:51 +0530

    Update FloatingOrnaments.tsx

--- components/FloatingOrnaments.tsx
+++ components/FloatingOrnaments.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-
 const SparklesIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
     <path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/>
