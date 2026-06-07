fb2dcc44327c98de7b82e22bfc7aa21e5a0b2d79
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2026-06-07 10:09:06 +0530

    Update EmailSignup.tsx

--- components/EmailSignup.tsx
+++ components/EmailSignup.tsx
@@ -21,7 +21,6 @@ const SparklesIcon = ({ className }: { className?: string }) => (
     <path d="M15 21h4"/>
   </svg>
 );
-
 const CrownIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
     <path d="m2 4 3 12h14l3-12-6 7-4-7-4 7-6-7zm3 16h14"/>
